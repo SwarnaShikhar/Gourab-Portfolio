a9530b53d6ad8170e56540f58d6e6731441a2c80
Swarna Shikhar Das <75322356+SwarnaShikhar@users.noreply.github.com>
2023-04-14 00:14:16 +0530

    Add files via upload

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Gourab Deb </span>
+            from <span className="purple"> Jalpaiguri, India.</span>
+            <br />I am an Undergrad currently doing my Btech from <span className="purple"> Siliguri Institute of Technology.</span>
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Video Editing
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Playing Guitar, Cajon
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/About/Techstack.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiJavascript1,
+  DiReact,
+  DiGit,
+  DiMysql,
+  DiHtml5,
+  DiCss3
+} from "react-icons/di";
+import { 
+  FaJava,
+  FaBootstrap
+} from "react-icons/fa";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaJava/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaBootstrap/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMysql/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
